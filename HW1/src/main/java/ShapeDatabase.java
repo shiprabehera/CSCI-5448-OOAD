8301e782ee314063f6fcb6c11bbc2b5bf477070a
Shipra Behera <shpr13@gmail.com>
2019-01-31 21:42:23 -0700

    move functions to shapedatabase

--- HW1/src/main/java/ShapeDatabase.java
+++ HW1/src/main/java/ShapeDatabase.java
@@ -1,7 +1,32 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class ShapeDatabase {
-    public void getCollection() {
-        //return
+    private List<Shape> shapes = new ArrayList<>();
+
+    public ShapeDatabase() {
+        shapes.add(new Circle(2, 2, 5));
+        shapes.add(new Square(0, 2, 5));
+        shapes.add(new Triangle(1, 2, 3, 4));
     }
+    public int getNumberOfShapes() {
+        return shapes.size();
+    }
+
+    public void sortShapes() {
+        //sorting the shapes based on x coordinate // can change this to anything
+        Collections.sort(shapes, Comparator.comparingDouble(Shape::getX));
+    }
+
+    public void displayShapes() {
+        //displaying each shape
+        for (Shape s: shapes) {
+            s.display();
+        }
+    }
+
 }
--- HW1/src/main/java/ShapeTest.java
+++ HW1/src/main/java/ShapeTest.java
@@ -4,19 +4,17 @@ import java.util.*;
 public class ShapeTest {
     public static void main(String args[]) {
         System.out.println("Hello World");
-        List<Shape> shapes = new ArrayList<>();
-        
-        shapes.add(new Circle(2, 2, 5));
-        shapes.add(new Square(0, 2, 5));
-        shapes.add(new Triangle(1, 2, 3, 4));
+        ShapeDatabase database = new ShapeDatabase();
 
-        //sorting the shapes based on x coordinate
-        Collections.sort(shapes, Comparator.comparingDouble(Shape::getX));
+        //is this correct?
+        System.out.println("Number of shapes are: "+ database.getNumberOfShapes());
 
-        //displaying each shape
-        for (Shape s: shapes) {
-            s.display();
-        }
+        //display unsorted shapes
+        database.displayShapes();
+        //sort
+        database.sortShapes();
+        //display unsorted shapes
+        database.displayShapes();
 
     }
 }
